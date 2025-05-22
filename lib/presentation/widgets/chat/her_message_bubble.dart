513f128e41c0c0e013a397dd4abf74ccb4358275
kevin <acosta.kevin@correounivalle.edu.co>
2025-05-22 10:22:34 -0500

    first commit

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  final Message message;
+  const HerMessageBubble({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+              bottomRight: Radius.circular(15),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child:
+                Text(message.text, style: const TextStyle(color: Colors.white)),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(imageUrl: message.imageUrl!),
+        const SizedBox(height: 15),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  final String imageUrl;
+
+  const _ImageBubble({required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Image.network(
+        imageUrl,
+        width: size.width * 0.5,
+        fit: BoxFit.cover,
+        height: 150,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// TODO: algo por hacer
+//! cosas importantes
+//? algo debe ser revisado o tengo una pregunta
+//* algo es importante pero no necesita atencion inmediata
